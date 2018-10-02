338609813745f7d46fefd94c35eafec48d163a11
jksdf <noroslivka@gmail.com>
2018-10-02 20:41:32 +0100

    fix

--- 01-regex/stat.py
+++ 01-regex/stat.py
@@ -9,17 +9,21 @@ def _year_to_century(year):
   c = year // 100
   return c if year % 100 == 0 else c + 1
 
+
+
 def _parse_century(data):
   data = data.strip()
   if not data:
     return None
-  match = re.match(r'^([0-9]+) ?th century$', data)
+  match = re.match(r'^([0-9]+) ?(th|st|nd) century$', data)
   if match:
     return int(match.group(1))
   match = re.match(r'.*([0-9]{4}).*', data)
   if match:
     return _year_to_century(int(match.group(1)))
   return None
+
+
 
 def _century_parse(f):
   r = re.compile(r'^Composition Year: +(.+)')
@@ -28,15 +32,32 @@ def _century_parse(f):
     match = r.match(line)
     if match:
       c = _parse_century(match.group(1))
-      if c is not None:
+      if c:
         centuries[c] += 1
   return centuries
+
+
+
+def _num_suffix(n):
+  if (n // 10) % 10 == 1:
+    return 'th'
+  if n % 10 == 1:
+    return 'st'
+  elif n % 10 == 2:
+    return 'nd'
+  else:
+    return 'th'
+
 
 
 def century(f):
   res = _century_parse(f)
   for century in sorted(res.keys()):
-    print('{}th century: {}'.format(century, res[century]))
+    print('{}{} century: {}'.format(century, 
+                                    _num_suffix(century),
+                                    res[century]))
+
+
 
 def _composer_parse(f):
   r = re.compile(r'Composer: (.*)')
@@ -45,28 +66,33 @@ def _composer_parse(f):
     match = r.match(line)
     if match:
       c = re.sub(r'\(.*?\)', '', match.group(1))
-      composers += {i.strip(): 1 for i in c.split(';')}
+      composers += {i.strip(): 1 for i in c.split(';') if i.strip()}
   return composers
       
+
 
 def composer(f):
   res = _composer_parse(f)
   for composer in sorted(res.keys()):
     print('{}: {}'.format(composer, res[composer]))
 
-COMMANDS = {'composer': composer, 'century': century, }
+
+
+
+
 
 def main():
+  commands = {'composer': composer, 'century': century, }
   if len(sys.argv) != 3:
     sys.stderr.write('Usage: ./stat.py scoreboard command.\n')
     sys.exit(1)
   commandName = sys.argv[2]
-  if commandName not in COMMANDS:
+  if commandName not in commands:
     sys.stderr.write('Given command "{}". '.format(commandName)
                      + 'Command has to be one of: '
-                     + ', '.join(['"{}"'.format(i) for i in COMMANDS]))
+                     + ', '.join(['"{}"'.format(i) for i in commands]))
     sys.exit(1)
-  command = COMMANDS[commandName]
+  command = commands[commandName]
   command(open(sys.argv[1]))
 
 if __name__ == '__main__':
