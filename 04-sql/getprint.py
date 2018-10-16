3267507b738bf3b21ce990d32165428de318861b
Norbert Sliva <noroslivka@gmail.com>
2018-10-16 22:41:43 +0200

    getPrint implemented

---
+++ 04-sql/getprint.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import json
+import sqlite3
+import sys
+from pprint import pprint
+
+
+def find(print_id):
+  connection = sqlite3.connect('scorelib.dat')
+  lst = []
+  for person in connection.execute(r'SELECT name, born, died FROM person WHERE id IN (SELECT composer FROM score_author WHERE score IN (SELECT score FROM main.edition WHERE id IN (SELECT print.edition FROM print WHERE print.id = ?)))', (print_id,)):
+    name, born, died = person
+    lst.append({'name': name, 'born': born, 'died': died})
+  json.dump(lst, sys.stdout, indent=4)
+
+
+def main(args):
+  find(int(args[0]))
+
+
+main(sys.argv[1:])
